3c5cab369c13e04a21482caaf84ae2f4ecffa2a3
Keifer May <dkmay3@gmail.com>
2023-05-16 21:17:39 -0400

    Begin working on darkmodeimplementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,67 @@
 // import logo from './logo.svg';
 // import styles from './App.module.css';
 
+import { createSignal } from 'solid-js';
 import banner from './assets/1080x1920.png'
 import Card from './components/card';
+import { root } from 'postcss';
 
 function App() { // solid idiom write functions in PascalCase()
+  const [darkTheme, setDarkTheme] = createSignal(false);
+
+  function toggleTheme() {
+    setDarkTheme(!darkTheme())
+  };
+
+
+
+  function updatingDocDark() {
+    let root = document.documentElement;
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+      if (localStorage.getItem('color-theme') === 'light') {
+          document.documentElement.classList.add('dark');
+          localStorage.setItem('color-theme', 'dark');
+          root.classList.add("theme-dark");
+      } else {
+          document.documentElement.classList.remove('dark');
+          localStorage.setItem('color-theme', 'light');
+          root.classList.remove("theme-dark");
+      }
+
+  // if NOT set via local storage previously
+  } else {
+      if (document.documentElement.classList.contains('dark')) {
+          document.documentElement.classList.remove('dark');
+          localStorage.setItem('color-theme', 'light');
+          root.classList.remove("theme-dark");
+      } else {
+          document.documentElement.classList.add('dark');
+          localStorage.setItem('color-theme', 'dark');
+          root.classList.add("theme-dark");
+      }
+  }
+  console.log(localStorage.getItem('color-theme'))
+  console.log(root.classList) // to do: learn how to conditionally render so that the DOM updates to dark mode, my localstorage and dom update properly, but the rendering is wonky!
+}
+  
+  
+
+
+
 
   // you can write other js code before your returned template
   return ( // your return, you have to return a single root element, such as a div or a fragment (EMpty html object <></>)
+    
     <div class="container">
-      <header>
+      
+      <header class="my-4 p-2 text-xl flex items-center gap-4">
+        <span class="material-symbols-outlined cursor-pointer"
+        onClick={() => updatingDocDark()}
+        >
+          light_mode
+        </span>
         <h1>Big Boi Merch</h1>
       </header>
 
@@ -54,5 +106,7 @@ function App() { // solid idiom write functions in PascalCase()
 
     );
 }
+
+// render(() => <ThemeToggler/>,document.getElementById("App")!);
 
 export default App;
